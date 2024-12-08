a89b051284811e8a3112e86e85074fc99b9c4882
Gabriela Moog <gmoog1999@gmail.com>
2024-12-08 00:16:44 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    $('#form-tarefa').on('submit', function(e) {
+        e.preventDefault(); 
+
+        const nomeTarefa = $('#nome-tarefa').val();
+
+        if (nomeTarefa.trim() !== '') {
+    
+            const novaTarefa = $('<li></li>').text(nomeTarefa);
+            
+            
+            $('#lista-tarefas').append(novaTarefa);
+            
+            
+            $('#nome-tarefa').val('');
+        }
+    });
+
+    
+    $('#botao-cancelar').click(function() {
+        $('#lista-tarefas').empty(); 
+    });
+
+    
+    $('#lista-tarefas').on('click', 'li', function() {
+        $(this).toggleClass('completed');
+    });
+});
