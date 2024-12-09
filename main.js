46f7283eea07cd3cad66847aeee77cc43cb2db36
Gabriela Moog <gmoog1999@gmail.com>
2024-12-08 23:04:44 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,80 @@
-$(document).ready(function() {
-    $('#form-tarefa').on('submit', function(e) {
-        e.preventDefault(); 
+$(document).ready(function () {
+    // Configuração do carousel
+    $('#carousel-imagens').slick({
+        autoplay: true,
+    });
 
-        const nomeTarefa = $('#nome-tarefa').val();
+    // Função para exibir/ocultar menu
+    $('.menu-hamburguer').click(function () {
+        $('nav').slideToggle();
+    });
 
-        if (nomeTarefa.trim() !== '') {
-    
-            const novaTarefa = $('<li></li>').text(nomeTarefa);
-            
-            
-            $('#lista-tarefas').append(novaTarefa);
-            
-            
-            $('#nome-tarefa').val('');
+    // Aplicando a máscara com o plugin 'JS'
+    $('#telefone').mask('(00) 00000-0000');
+
+    // Configuração da validação do formulário com jQuery Validate
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true,
+                minlength: 3
+            },
+            email: {
+                required: true,
+                email: true,
+            },
+            telefone: {
+                required: true
+            },
+            veiculoDeInteresse: { 
+                required: false,
+            },
+            mensagem: {
+                required: true,
+                minlength: 10
+            }
+        },
+        messages: {
+            nome: {
+                required: "Por favor, insira seu nome",
+                minlength: "O nome deve ter pelo menos 3 caracteres"
+            },
+            email: {
+                required: "Por favor, insira seu e-mail",
+                email: "Por favor, insira um e-mail válido"
+            },
+            telefone: {
+                required: "Por favor, insira seu telefone"
+            },
+            veiculoDeInteresse: {
+                required: "Por favor, insira o veículo de interesse"
+            },
+            mensagem: {
+                required: "Por favor, insira uma mensagem",
+                minlength: "A mensagem deve ter pelo menos 10 caracteres"
+            }
+        },
+        submitHandler: function(form) {
+            alert('Formulário enviado com sucesso!');
+            form.submit();
+        },
+        invalidHandler: function(event, validator) {
+            let camposIncorretos = validator.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`);
+            }
         }
     });
 
     
-    $('#botao-cancelar').click(function() {
-        $('#lista-tarefas').empty(); 
-    });
+    $('.lista-veiculos button').click(function () {
+        const destino = $('#contato');
+        const nomeVeiculo = $(this).parent().find('h3').text();
 
-    
-    $('#lista-tarefas').on('click', 'li', function() {
-        $(this).toggleClass('completed');
+        $('#veiculo-interesse').val(nomeVeiculo);
+
+        $('html, body').animate({ 
+            scrollTop: destino.offset().top
+        }, 1000);
     });
 });
